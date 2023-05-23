1a3a56664bf807b3dfdbc0bc2b9ce3cab348567c
mbasa <mario.basa@gmail.com>
2023-05-23 14:33:47 +0800

    added a go get

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export CGO_CFLAGS="-I`pg_config --includedir-server` "
+go get  github.com/RyanCarrier/dijkstra
 go build -buildmode=c-shared -o libPgDijkstra-go.so
-
-#CGO_CFLAGS="-I/opt/homebrew/Cellar/postgresql@14/14.6/include/postgresql@14/server/" go build -buildmode=c-shared -o libGoPgFunc.so
